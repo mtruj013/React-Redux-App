2503cf949f369c4c535f8d9ce8ef9757324ba664
Maria Trujillo <mptruj015@gmail.com>
2020-04-15 21:56:15 +0200

    set up file structure and set initial start in reducer

---
+++ app/src/store/reducers/sortingReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    result: '',
+    isLoading: false,
+    error: '' 
+}
+
+export const sortingReducer = (state = initialState, action) => {
+    switch (action.type){
+        default:
+            return state;
+    }
+}
